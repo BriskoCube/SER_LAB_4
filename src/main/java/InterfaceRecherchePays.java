dccc4ef021f944175776a29e2bc4710d5618ca41
Julien Quartier <julien.quartier@bluewin.ch>
2019-05-28 09:25:19 +0200

    Fill ComboBox with continents and languages

--- src/main/java/InterfaceRecherchePays.java
+++ src/main/java/InterfaceRecherchePays.java
@@ -1,20 +1,59 @@
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.filter.Filters;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.xpath.XPathExpression;
+import org.jdom2.xpath.XPathFactory;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.File;
+import java.io.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class InterfaceRecherchePays extends JFrame {
 
     private JPanel panelRecherche = new JPanel(new FlowLayout());
 
-    private JComboBox<String> continents = new JComboBox<>();
-    private JComboBox<String> langages = new JComboBox<>();
+    private JComboBox<Object> continents;
+    private JComboBox<Object> langages;
     private JButton createXSL = new JButton("Générer XSL");
     private JTextField superficieMin = new JTextField(5);
     private JTextField superficieMax = new JTextField(5);
 
     public InterfaceRecherchePays(File xmlFile) {
+
+        try{
+            InputStream in = new FileInputStream(xmlFile);
+            SAXBuilder builder = new SAXBuilder();
+            Document document = builder.build(in);
+
+
+            XPathFactory xpath = XPathFactory.instance();
+
+
+            Set<String> languages = xPathQuery("/countries/element/languages/element/name", xpath, document);
+            Set<String> continents = xPathQuery("/countries/element/region", xpath, document);
+
+
+            this.langages = new JComboBox<Object>(languages.toArray());
+            this.continents = new JComboBox<Object>(continents.toArray());
+
+
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JDOMException e) {
+            e.printStackTrace();
+        }
+
 
         createXSL.addMouseListener(new MouseAdapter() {
 
@@ -22,6 +61,16 @@ public class InterfaceRecherchePays extends JFrame {
             public void mouseClicked(MouseEvent e) {
 
                 super.mouseClicked(e);
+
+                //distinct-values(/countries/element/languages/element/name)
+
+                //distinct-values(/countries/element/region)
+
+
+
+
+
+
 
                 // Création des fichiers XSL selon ce qui est demandé
 
@@ -59,6 +108,22 @@ public class InterfaceRecherchePays extends JFrame {
         setTitle("Interface de recherche de pays");
 
 
+    }
+
+    public Set<String> xPathQuery(String expressions, XPathFactory xpath, Document document){
+        XPathExpression<Element> expr = xpath.compile(expressions, Filters.element());
+
+        List<Element> duplicatedElements = expr.evaluate(document);
+
+        Set<String> elements = new HashSet<>();
+
+        elements.add("");
+
+        for (Element element : duplicatedElements){
+            elements.add(element.getValue().trim());
+        }
+
+        return elements;
     }
 
     public static void main(String ... args) {
